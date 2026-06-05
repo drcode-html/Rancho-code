d920486dc03de99f4f94214cc26f1030989dcaed
drcode-html <advayagarwal1902@gmail.com>
2026-06-05 10:22:55 +0530

    finished web scrape code

--- webscraping.py
+++ webscraping.py
@@ -2,3 +2,5 @@ import requests
 from bs4 import BeautifulSoup
 url = "https://pokemondb.net/pokedex/all"
 page = requests.get(url)
+soup = BeautifulSoup(page.txt , "html.parser")
+print(soup.title.text)
