66f6bf3c0092d21d893efc8448d4668e04b0e690
drcode-html <advayagarwal1902@gmail.com>
2026-06-06 10:10:43 +0530

    new file for streamlit website

---
+++ myapp.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.title("My first steamlit app")
+st.header("Hello this is a Header")
+st.subheader("Hello this is a subheader")
+st.write("st.write is used to write any normal text")
+st.markdown("markdown allows for even more features like using html ")
+
+
+
