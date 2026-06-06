811f0b8947ad2296636559d858df8bbd06b79f17
drcode-html <advayagarwal1902@gmail.com>
2026-06-06 10:53:42 +0530

    just a update

--- myapp.py
+++ myapp.py
@@ -3,7 +3,7 @@ st.title("My first steamlit app")
 st.header("Hello this is a Header")
 st.subheader("Hello this is a subheader")
 st.write("st.write is used to write any normal text")
-st.markdown("markdown allows for even more features like using html ")
+st.markdown("markdown allows for even more features like using html s")
 
 
 
